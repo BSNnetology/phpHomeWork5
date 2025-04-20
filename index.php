dcca96782fb006e4994280a642408f174e5be78f
Sergei Belash <BelashSN@outlook.com>
2025-04-20 16:13:41 +0300

    Add calculation

--- index.php
+++ index.php
@@ -19,39 +19,34 @@ if ($year < 2000 || $year > 2050) {
 }
 
 showCalendar($year, $month, $monthCount);
+echo "\n";
 
 // -------------------
 function showCalendar(int $year, int $month, int $monthCount): void {
-    $dateStart = mktime(0, 0, 0, $month, 1, $year);
+    $dateStart = mktime(0, 0, 0, $month, 1, $year);   
+    $workDay = 1;
 
     for ($i = 0; $i < $monthCount; $i++) {
         $dateMonth = strtotime('+' . $i . ' month', $dateStart);
         showCalendarHeader($dateMonth);
 
+        $workDay = isset($daysInMouth) ? $workDay - $daysInMouth : 1;
+        $daysInMouth = (int) date('t', $dateMonth);
         $weekDay = (int) date('w', $dateMonth);
-        echo str_repeat("\t", $weekDay === 0 ? 6 : $weekDay - 1);
 
+        echo str_repeat("\t", $weekDay === 0 ? 6 : $weekDay - 1);
         $weekDay = $weekDay === 0 ? 7 : $weekDay;
+
         for ($j = 1; $j <= (int) date('t', $dateMonth); $j++) {
             if ($weekDay > 7) {
                 $weekDay = 1;
                 echo "\n";
             }
-
+            
             $date = $j < 10 ? " $j" : $j;
-            $result = getDayStatus($dateMonth, $j, $weekDay);
+            $result = getDayStatus($dateMonth, $j, $weekDay, $workDay);
+            showCalendarDay($date, $result);
 
-            switch ($result) {
-                case -1:
-                    echo "\033[31m$date\t\033[0m";
-                    break;
-                case 1:
-                    echo "\033[32m$date\t\033[0m";;
-                    break;
-                default:
-                    echo "$date\t";
-                    break;
-            }
             $weekDay++;
         }
         echo "\n";
@@ -66,14 +61,47 @@ function showCalendarHeader(int $dateMonth): void {
 }
 
 // -------------------
-function getDayStatus(int $dateMonth, int $j, int $weekDay): int {
-    $currentDate = new DateTime('@' . strtotime('+' . ($j - 1) . ' day', $dateMonth));
+function getCurrentDate(int $j, int $dateMonth): DateTime {
+    return new DateTime('@' . strtotime('+' . ($j - 1) . ' day', $dateMonth));
+}
 
-    if ($weekDay > 5 || in_array($currentDate->format('*-m-d'), HOLIDAYS)) {
+// -------------------
+function showCalendarDay(string $date, int $result): void {
+    switch ($result) {
+        case -1:
+            echo "\033[31m$date\t\033[0m";
+            break;
+        case 1:
+            echo "\033[32m$date\t\033[0m";;
+            break;
+        default:
+            echo "$date\t";
+            break;
+    }
+}
+
+// -------------------
+function checkHoligay(int $weekDay, DateTime $currentDate): bool {
+    return ($weekDay > 5) || (in_array($currentDate->format('*-m-d'), HOLIDAYS));
+}
+
+// -------------------
+function getDayStatus(int $dateMonth, int $j, int $weekDay, int &$workDay): int {   
+    $currentDate = getCurrentDate($j, $dateMonth);
+    $isHoliday = checkHoligay($weekDay, $currentDate);
+    $isWorkDay = ($j === $workDay);
+
+    if (($j === $workDay) && $isHoliday) {
+        $workDay++;
+    }
+
+    if ($isHoliday ) {
         return -1;
-    } if ($weekDay > 15) {
+    }  elseif ($isWorkDay) {
+        $workDay += 3;
         return 1;
     } else {
         return 0;
     }
 }
+
